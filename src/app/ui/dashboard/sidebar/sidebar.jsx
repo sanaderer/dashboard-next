3d143f4bb225cd78bcb99a2d5bff55d56ed372b9
Sabrina Naderer <95416508+sanaderer@users.noreply.github.com>
2024-01-30 14:52:02 -0500

    sidebar

--- src/app/ui/dashboard/sidebar/sidebar.jsx
+++ src/app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,11 +1,32 @@
+import React from 'react';
+import styles from './sidebar.module.css';
+import Image from 'next/image';
+
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <div className="sidebar__header">
-        <h3>Sidebar</h3>
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <Image src="/logo.png" alt="logo" width="36" height="36" />
       </div>
+      <hr className={styles.divider}/>
+      <NavItem icon="/icon-1.png" alt="user" />
+      <NavItem icon="/icon-2.png" alt="upload" />
+      <NavItem icon="/icon-3.png" alt="analytics" />
+      <NavItem icon="/icon-4.png" alt="images" />
+      <NavItem icon="/icon-5.png" alt="calendar" />
+      <NavItem icon="/icon-8.png" alt="settings" className={styles.settings} />
+      <ul>
+      </ul>
     </div>
   );
-}
+};
+
+const NavItem = ({ icon, alt, className }) => {
+  return (
+    <div className={`${styles.navItem} ${className}`}>
+      <Image src={icon} alt={alt} width="26" height="26" />
+    </div>
+  );
+};
 
 export default Sidebar;
