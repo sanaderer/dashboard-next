606b5c3e189b2193e25e1aa226c54970ed334bfc
Sabrina Naderer <95416508+sanaderer@users.noreply.github.com>
2024-01-31 14:51:48 -0500

    blog

--- src/app/dashboard/blog/page.jsx
+++ src/app/dashboard/blog/page.jsx
@@ -1,9 +1,28 @@
+import styles from '/src/app/ui/dashboard/blog/blog.module.css';
+
 const BlogPage = () => {
   return (
-    <div>
-      <h1>Blog Page</h1>
+    <div className={styles.centeredContainer}>
+      <div className={styles.card}>
+        <div className={styles.tagContainer}>
+          <textarea placeholder="Tag" maxLength={10} className={styles.tag} />
+          <textarea placeholder="Tag" maxLength={10} className={styles.tag} />
+        </div>
+        <textarea placeholder="Title" className={styles.title} maxLength={20} />
+        <textarea placeholder="Description" className={styles.description} maxLength={100} />
+        <textarea placeholder="Details" className={styles.details} maxLength={1000} />
+        <div className={styles.button}>
+          <button className={styles.save}>Save</button>
+          <div className={styles.image}>
+            <label>
+              Choose File
+              <input type="file" />
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default BlogPage;
