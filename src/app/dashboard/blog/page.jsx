4afdad7d3bcf840e78288a40224e46ff818e065b
Sabrina Naderer <95416508+sanaderer@users.noreply.github.com>
2024-01-31 15:17:11 -0500

    card form update

--- src/app/dashboard/blog/page.jsx
+++ src/app/dashboard/blog/page.jsx
@@ -2,15 +2,15 @@ import styles from '/src/app/ui/dashboard/blog/blog.module.css';
 
 const BlogPage = () => {
   return (
-    <div className={styles.centeredContainer}>
-      <div className={styles.card}>
+    <div className={styles.container}>
+      <form action="" className={styles.card}>
         <div className={styles.tagContainer}>
-          <textarea placeholder="Tag" maxLength={10} className={styles.tag} />
-          <textarea placeholder="Tag" maxLength={10} className={styles.tag} />
+          <input type="text" placeholder="Tag" name="tag" maxLength={10} className={styles.tag} required />
+          <input type="text" placeholder="Tag" name="tag" maxLength={10} className={styles.tag} />
         </div>
-        <textarea placeholder="Title" className={styles.title} maxLength={20} />
-        <textarea placeholder="Description" className={styles.description} maxLength={100} />
-        <textarea placeholder="Details" className={styles.details} maxLength={1000} />
+        <input type="text" placeholder="Title" name="title" className={styles.title} maxLength={20} required />
+        <textarea placeholder="Description" name="desc" id="desc" rows="10" className={styles.description} maxLength={100} required />
+        <textarea placeholder="Details" name="details" className={styles.details} maxLength={1000} required />
         <div className={styles.button}>
           <button className={styles.save}>Save</button>
           <div className={styles.image}>
@@ -20,7 +20,7 @@ const BlogPage = () => {
             </label>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
