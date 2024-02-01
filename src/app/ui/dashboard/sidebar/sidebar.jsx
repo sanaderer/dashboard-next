304cf48b22c0e6286bf4efa3a9e12ee4b86c7b71
Sabrina Naderer <95416508+sanaderer@users.noreply.github.com>
2024-02-01 15:00:47 -0500

    update

--- src/app/ui/dashboard/sidebar/sidebar.jsx
+++ src/app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from './sidebar.module.css'
-import Image from 'next/image'
 import Link from 'next/link'
 import UserSvg from '/src/svgs/UserSvg'
 import UploadSvg from '/src/svgs/UploadSvg'
@@ -20,25 +19,19 @@ const Sidebar = () => {
         <LogoSvg width={36} />
       </Link>
       </div>
-      <hr className={styles.divider}/>
-      <UserSvg width={26} />
-      <UploadSvg width={26}/>
-      <AnalyticsSvg width={26} />
-      <ImageSvg width={26} />
-      <CalendarSvg width={26} />
-      <HotSvg width={26} />
-      <DataSvg width={26} />
-      <SettingSvg width={26} className={styles.settings} />
-      <ul>
-      </ul>
-    </div>
-  );
-};
-
-const NavItem = ({ icon, alt, className }) => {
-  return (
-    <div className={`${styles.navItem} ${className}`}>
-      <Image src={icon} alt={alt} width="20" height="20" />
+      <hr className={styles.divider}/> 
+      <div className={styles.navItem}>
+        <UserSvg width={26} />
+        <UploadSvg width={26}/>
+        <AnalyticsSvg width={26} />
+        <ImageSvg width={26} />
+        <CalendarSvg width={26} />
+        <HotSvg width={26} />
+        <DataSvg width={26} />
+      </div>
+      <div className={styles.navbar}>
+        <SettingSvg width={26} className={styles.settings} />
+      </div>
     </div>
   );
 };
