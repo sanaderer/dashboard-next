0ca9e597b7550ccdf515c1504a294c14ac28e1ef
Victor Saisse <saissevictor@gmail.com>
2024-02-01 11:48:10 -0500

    svg

--- src/app/ui/dashboard/navbar/navbar.jsx
+++ src/app/ui/dashboard/navbar/navbar.jsx
@@ -1,14 +1,14 @@
 // navbar.jsx
-import styles from './navbar.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
+import SearchSvg from "@/src/svgs/SearchSvg";
+import styles from "./navbar.module.css";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.search}>
-        <Image src="/search.png" alt="logo" width="24" height="24" />
-        <input type="text" placeholder="Search" className={styles.input}/>
+        <SearchSvg widht={24} />
+        <input type="text" placeholder="Search" className={styles.input} />
       </div>
       <div className={styles.addButton}>
         <Link href="/dashboard/blog">
@@ -16,7 +16,7 @@ const Navbar = () => {
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
