7cbfc1a6b29230ef86042e65ddf2afa41e95d8b4
Sabrina Naderer <95416508+sanaderer@users.noreply.github.com>
2024-02-02 13:45:46 -0500

    radom colors for the tags

--- src/app/ui/dashboard/card/card.jsx
+++ src/app/ui/dashboard/card/card.jsx
@@ -1,11 +1,27 @@
 import styles from './card.module.css';
 
+const predefinedColors = ['#FFE5D3', '#DAD7F', '#E2FBD7', '#FFE5D3', '#FFF5CC', '#CCF8FE'];
+
+const shuffleArray = (array) => {
+  const shuffledArray = [...array];
+
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+};
+
 const Card = ({ id, tags, title, description, details, image }) => {
+  const shuffledColors = shuffleArray(predefinedColors);
   return (
     <div className={styles.container}>
       <div className={styles['card-text']}>
         {tags.map((tag, index) => (
-          <div key={index} className={styles['element-text']}>
+          <div 
+            key={index} 
+            className={styles['element-text']}
+            style={{background: shuffledColors.pop()}}>
             {tag}
           </div>
         ))}
